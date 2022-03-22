2fde7375d9c78205fbbf0c59b785487c341d0ead
Meija Lohiniva <mlohiniva@hotmail.com>
2022-03-22 13:28:21 +0200

    User-> person. Added worktime

--- sql_scipts/db.sql
+++ sql_scipts/db.sql
@@ -1,6 +1,19 @@
+-- CREATE TABLES
+
+-- Person table
 DROP TABLE IF EXISTS person;
 CREATE TABLE person(  
     ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     firstname VARCHAR(255),
     lastname VARCHAR(255)
+);
+
+-- Worktime table
+DROP TABLE IF EXISTS worktime;
+CREATE TABLE worktime(  
+    ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    start_time DATETIME,
+    end_time DATETIME,
+    person_id INT FOREIGN KEY REFERENCES person(ID),
+    task_description TEXT
 );
