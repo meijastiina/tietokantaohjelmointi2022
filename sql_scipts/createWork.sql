08c222e34725125377cb22bd37bd11a5d3f44564
repsukka <81526112+repsukka@users.noreply.github.com>
2022-03-23 00:14:34 +0200

    Deleted unnecessary sql script

--- sql_scipts/createWork.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS work_time;
-CREATE TABLE work_time(
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    starttime DATETIME,
-    endtime DATETIME,
-    person_id int,
-    CONSTRAINT `fk_person`
-    FOREIGN KEY (person_id) REFERENCES person(id)
-);
