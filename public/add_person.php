6b27acd1ff9faf68f32b1f56cb9a58e442b0a7e4
repsukka <81526112+repsukka@users.noreply.github.com>
2022-03-30 13:04:43 +0300

    Credentials added

--- public/add_person.php
+++ public/add_person.php
@@ -5,25 +5,32 @@ require '../src/modules/db.php'; // DB connection
 //Jos parametria ei löydy, funktio palauttaa null
 $fname = filter_input(INPUT_POST, "fname");
 $lname = filter_input(INPUT_POST, "lname");
+$uname = filter_input(INPUT_POST, "username");
+$pw = filter_input(INPUT_POST, "password");
 
 //Tarkistetaan onko muttujia asetettu
-if( !isset($fname) || !isset($lname) ){
+if( !isset($fname) || !isset($lname) || !isset($uname) || !isset($pw) ){
     echo "Parametreja puuttui!! Ei voida lisätä henkilöä";
     exit;
 }
 
 //Tarkistetaan, ettei tyhjiä arvoja muuttujissa
-if( empty($fname) || empty($lname) ){
+if( empty($fname) || empty($lname) || empty($uname) || empty($pw) ){
     echo "Et voi asettaa tyhjiä arvoja!!";
     exit;
 }
 
 try{
     //Suoritetaan parametrien lisääminen tietokantaan.
-    $sql = "INSERT INTO person (firstname, lastname) VALUES (?, ?)";
+    $sql = "INSERT INTO person (firstname, lastname, username, password) VALUES (?, ?, ?, ?)";
     $statement = $pdo->prepare($sql);
     $statement->bindParam(1, $fname);
     $statement->bindParam(2, $lname);
+    $statement->bindParam(3, $uname);
+
+    $hash_pw = password_hash($pw, PASSWORD_DEFAULT);
+    $statement->bindParam(4, $hash_pw);
+    
 
     $statement->execute();
 
--- public/person.php
+++ public/person.php
@@ -5,6 +5,10 @@ include('../src/templates/head.php');
         <input type="text" name="fname" id="fname"><br>
         <label for="lname">Last name:</label><br>
         <input type="text" name="lname" id="lname"><br>
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password"><br>
         <input type="submit" class="btn btn-primary" value="Add person">
     </form>';
 include('../src/templates/foot.php');
