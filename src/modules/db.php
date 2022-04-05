2c4494bc93e645d26f9a5a930b88e2f782facca1
repsukka <81526112+repsukka@users.noreply.github.com>
2022-04-05 10:37:43 +0300

    Connection as function and path update

--- src/modules/db.php
+++ src/modules/db.php
@@ -5,20 +5,22 @@
 //Voit käyttää myös ylempänä olevaa config.php tiedostoa
 //jos haluat. Kommentoi silloin seuraavat rivit.
 
-//Vaihda work tilalle oma juurikansio (localhost/work/....)
-//Suora relatiivinen polku kuten ../config/conf.ini ei toimi
-//koska tämä sijainti riippuu siitä mihin selaimelta kutsuttuun 
-//sijaintiin db.php on kytketty (kuten esim. public/people.php tai scr/modules/add_worktime.php)
-$init = parse_ini_file($_SERVER['DOCUMENT_ROOT']."/work/conf.ini");
-$host = $init["host"];
-$db = $init["db"];
-$user = $init["username"];
-$password = $init["pw"];
+function getPdoConnection(){
+	$init = parse_ini_file(BASE_DIR."myconf.ini");
+	$host = $init["host"];
+	$db = $init["db"];
+	$user = $init["username"];
+	$password = $init["pw"];
+	
+	$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
-try {
-	$pdo = new PDO($dsn, $user, $password);
-    //echo "Connected!";
-} catch (PDOException $e) {
-	echo $e->getMessage();
+	try {
+		$pdo = new PDO($dsn, $user, $password);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	return $pdo;
 }
+
+
