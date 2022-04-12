3f102436d71c22b7c4630aa23904df09d31d3f2b
Meija Lohiniva <mlohiniva@hotmail.com>
2022-04-12 15:00:05 +0300

    Added monthly totals report

--- public/worklist.php
+++ public/worklist.php
@@ -2,21 +2,39 @@
 include TEMPLATES_DIR."head.php";
 include MODULES_DIR."worktime.php";
 
-// Get all worktime from database
-$worktimelist = getWorktimeReport();
+// Get all worktimes from database
+$workTimes = getWorktimeReport();
+// Get monthly totals from database
+$monthlyTotals = getMonthlyTotals();
 ?>
+<h2>All Worktimes</h2>
 <table class="table table-striped">
     <tr>
+        <th>Lastname</th>
+        <th>First name</th>
         <th>Start time</th>
         <th>End time</th>
+        <th>Duration</th>
     </tr>
 <?php
-foreach($worktimelist as $worktime){
-    echo "<tr><td>".$worktime["start_time"]."</td><td>".$worktime["end_time"].'</td></tr>';
+foreach($workTimes as $workTime){
+    echo "<tr><td>".$workTime["lastname"]."</td><td>" . $workTime["firstname"]."</td><td>" . $workTime["start_time_formatted"]."</td><td>".$workTime["end_time_formatted"].'</td><td>' . $workTime["duration"] . '</tr>';
 }
 ?>
 
-</table>;
+</table>
+<h2>Monthly Totals</h2>
+<table class="table table-striped">
+    <tr>
+        <th>Month</th>
+        <th>Total Hours</th>
+    </tr>
+    <?php
+        foreach($monthlyTotals as $month){
+            echo "<tr><td>".$month["month"]."</td><td>" . $month["MonthlyTotalHours"]."</td></tr>";
+        }
+    ?>
+</table>
 <?php
 include TEMPLATES_DIR.'foot.php';
 ?>
--- src/modules/worktime.php
+++ src/modules/worktime.php
@@ -36,11 +36,38 @@ function getWorktimeReport(){
     try{
         $pdo = getPdoConnection();
         // Create SQL query to get all rows from a table
-        $sql = "SELECT start_time, end_time FROM worktime";
+        $sql = "SELECT 
+        lastname,
+        firstname,
+        DATE_FORMAT(start_time, '%d.%m.%Y %H:%i') AS start_time_formatted, 
+        DATE_FORMAT(end_time, '%d.%m.%Y %H:%i') AS end_time_formatted,
+        TIMEDIFF(end_time, start_time) AS duration
+    FROM worktime INNER JOIN person ON worktime.person_id= person.ID
+    ORDER BY lastname, firstname, start_time;";
         // Execute the query
         $worktime = $pdo->query($sql);
 
         return $worktime->fetchAll();
+    }catch(PDOException $e){
+        throw $e;
+    }
+}
+function getMonthlyTotals(){
+    require_once MODULES_DIR.'db.php';
+
+    try{
+        $pdo = getPdoConnection();
+        // Create SQL query to get all rows from a table
+        $sql = "SELECT 
+        MONTH(start_time) AS month,
+        ROUND(SUM(TIME_TO_SEC(TIMEDIFF(end_time, start_time))/3600), 2) AS MonthlyTotalHours
+    FROM worktime 
+    GROUP BY MONTH(start_time)
+    ;";
+        // Execute the query
+        $monthlyTotals = $pdo->query($sql);
+
+        return $monthlyTotals->fetchAll();
     }catch(PDOException $e){
         throw $e;
     }
